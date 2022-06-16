a5abf35ed69892cae81151b6aac8b9a849037bd7
RyanShumway <ryanshumway@duck.com>
2022-06-16 08:22:48 -0600

    added NavBar + moved files around

---
+++ ryan-porfolio/src/components/App.js
@@ -0,0 +1,25 @@
+import '../css/App.css';
+import {NavBar} from './NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <NavBar />
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Butthole
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ryan-porfolio/src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from 
+
+function NavBar() {
+    return <h1>Here's the nav bar!</h1>
+}
+
+export {NavBar}
